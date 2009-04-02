c3ab9dae59b9ef7462aa815599648e564e7d16c0
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2009-04-02 10:00:43 +0200

    Prefix current branch name by "refs/heads" before looking for ref
    This allow to handle the case where local branch name is the same as remote branch name.

--- gitg/gitg-repository.c
+++ gitg/gitg-repository.c
@@ -753,6 +753,10 @@ static void
 load_refs(GitgRepository *self)
 {
 	gchar *current = load_current_ref(self);
+	// FIXME quite gruik
+	// We have to compare absolute refname in order to avoid case where local branch name
+	// copy a remote branch name.
+	gchar *current_refname = g_strconcat("refs/heads/", current, NULL);
 	
 	gchar **refs = gitg_repository_command_with_outputv(self, NULL, "for-each-ref", "--format=%(refname) %(objectname)", "refs", NULL);
 	gchar **buffer = refs;
@@ -767,7 +771,7 @@ load_refs(GitgRepository *self)
 		{
 			GitgRef *ref = add_ref(self, components[1], components[0]);
       
-			if (g_str_has_suffix(components[0], current))
+			if (strcmp(components[0], current_refname) == 0)
 				self->priv->current_ref = gitg_ref_copy(ref);
 		}
 		
@@ -776,6 +780,7 @@ load_refs(GitgRepository *self)
 
 	g_strfreev(refs);
 	g_free(current);
+	g_free(current_refname);
 }
 
 void
